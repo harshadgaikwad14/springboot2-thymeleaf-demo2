0f85f30c20d7c8603f0b163991a402d8d8fc16e2
harshag@00389 <harshadgaikwad14@gmail.com>
2020-12-12 10:50:05 +0530

    First Version

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String root() {
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public String userIndex() {
+        return "user";
+    }
+
+    @GetMapping("/manager")
+    public String manager() {
+        return "manager";
+    }
+    @GetMapping("/starter")
+    public String starter() {
+        return "starter";
+    }
+   
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/access-denied")
+    public String accessDenied() {
+        return "error/access-denied";
+    }
+
+}
